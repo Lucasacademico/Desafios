ca930bf48de44663a54d211d446607aebbe3de5a
Lucasacademico <100856355+Lucasacademico@users.noreply.github.com>
2023-01-04 12:35:35 -0400

    Update 1042 - Simple Sort.py

--- Beecrowd - Python/1042 - Simple Sort.py
+++ Beecrowd - Python/1042 - Simple Sort.py
@@ -1,5 +1,15 @@
-a, b, c = map(float, input().split())
+'''
+Read three integers and sort them in ascending order. After, print these values in ascending order, a 
+blank line and then the values in the sequence as they were readed.
 
+Input
+The input contains three integer numbers.
+
+Output
+Present the output as requested above.
+'''
+
+a, b, c = map(float, input().split())
 if((a + b > c) and(b + c > a) and (a + c > b)):
     p = a + b + c
     print(f'Perimetro = {p:.1f}')
